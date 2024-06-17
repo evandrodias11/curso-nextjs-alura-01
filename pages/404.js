3802b73ad7057e6ef467ff4c55f5a4bea6be6f61
evandrodias11 <evandro.dias@paripassu.com.br>
2024-06-17 08:20:56 -0300

    first commit

---
+++ pages/404.js
@@ -0,0 +1,10 @@
+import Link from "../src/components/Link";
+
+export default function Page404() {
+  return (
+    <div>
+      <h1>Voce se perdeu e caiu na página 404</h1>
+      <Link href="/">ir para home</Link>
+    </div>
+  );
+}
---
+++ pages/faq.js
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+import Link from "../src/components/Link";
+import Head from "next/head";
+
+export async function getStaticProps() {
+  const FAQ_API_URL =
+    "https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json";
+  const faq = await fetch(FAQ_API_URL)
+    .then((respostaServidor) => {
+      return respostaServidor.json();
+    })
+    .then((resposta) => {
+      return resposta;
+    });
+
+  return {
+    props: {
+      faq,
+    },
+  };
+}
+
+export default function FAQPage({ faq }) {
+  return (
+    <div>
+      <Head>
+        <title>FAQ - Alura Cases</title>
+      </Head>
+      <h1>Alura Cases - FAQ</h1>
+      <Link href="/">ir para home</Link>
+      <ul>
+        {faq.map(({ answer, question }) => (
+          <li key={question}>
+            <article>
+              <h2>{question}</h2>
+              <p>{answer}</p>
+            </article>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
